74a8a83c2461091bd60b798ae663e195080e5a15
Michael McCord <inquiries@michaelmccord.me>
2020-04-25 12:21:34 -0400

    test(get-package): Adds test for get-package

---
+++ spec/unit-tests/get-package.spec.js
@@ -0,0 +1,31 @@
+const getPackageFn = require('../../src/get-package');
+const sinon = require('sinon');
+const requireg = require('requireg');
+const assert = require('assert');
+
+afterEach(function(){
+  sinon.reset();
+});
+
+describe('get-package', function(){
+  it('calls require', function(){
+    const requireSpy = sinon.spy();
+    const requiregSpy = sinon.spy();
+    const result = getPackageFn(requireSpy, requiregSpy)('spec-md');
+    assert(requireSpy.withArgs('spec-md').calledOnce);
+  });
+
+  it('calls requireg', function(){
+    const requireSpy = sinon.stub();
+    const requiregSpy = sinon.stub();
+    const result = getPackageFn(requireSpy, requiregSpy)('spec-md');
+    assert(requiregSpy.withArgs('spec-md').calledOnce);
+  });
+
+  it('does not call requireg if found on require', function(){
+    const requireSpy = sinon.stub().returns(true)
+    const requiregSpy = sinon.stub();
+    const result = getPackageFn(requireSpy, requiregSpy)('spec-md');
+    assert(requiregSpy.notCalled);
+  });
+});
