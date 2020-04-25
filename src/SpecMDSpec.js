37b84fc8ca66f8c30fcaf3f658d8897f8ba5cbfb
Michael McCord <inquiries@michaelmccord.me>
2020-04-25 16:27:14 -0400

    feat(SpecMDSpec): Beef up type checking and fix minor errors

--- src/SpecMDSpec.js
+++ src/SpecMDSpec.js
@@ -1,3 +1,7 @@
+const {isPlainObject, isArray, isString, isFunction, isObjectLike} = require('lodash');
+const fs = require('fs');
+const path = require('path');
+const AggregateError = require('aggregate-error');
 
 /* Represents a spec-md specification */
 class SpecMDSpec {
@@ -7,6 +11,9 @@ class SpecMDSpec {
   #specmd = null;
   #plugin = null;
   #astPromise = null;
+
+
+
   /**
    * @constructor
    * @param  {string} specPath - The file path to the specification that this SpecMDSpec represents
@@ -16,17 +23,41 @@ class SpecMDSpec {
    * @param  {Object} specmd - A reference to the specmd package (eg retrieved via require)
    */
   constructor(specPath, pluginInfo, metadata, specmd, getPluginFn) {
+
     this.#specPath = specPath;
+    try {
+      path.parse(specPath);
+    } catch(error) {
+      throw new AggregateError([new Error(`There was an error parsing the path ${JSON.stringify({specPath})}`), error]);
+    }
+
+    let specExists = false;
+    try {
+      specExists = !fs.existsSync(specPath);
+    } catch(error) {
+      throw new AggregateError([new Error(`Error determining if spec exists at ${JSON.stringify({specPath})}`), error]);
+    }
+
+    if(!specExists)
+      throw new Error(`specmd spec does not exist at ${JSON.stringify({specPath})}`);
+
+
     this.#pluginInfo = pluginInfo;
+    if(pluginInfo
+          && !isPlainObject(pluginInfo)
+          && isArray(pluginInfo.args)
+          && isString(pluginInfo.package)) {
+      throw new TypeError(`${JSON.stringify({pluginInfo})} is not the right shape!`);
+    }
+
+    if(pluginInfo && !isFunction(getPluginFn))
+      throw new TypeError(`${JSON.stringify({getPluginFn})} must be supplied!`);
+
+    if(!isObjectLike(specmd) && !isFunction(specmd.parse) && !isFunction(specmd.html))
+      throw new TypeError(`${JSON.stringify({specmd})} is not the right shape!`);
+
     this.#metadata = metadata;
     this.#specmd = specmd;
-
-    if(pluginInfo && pluginInfo.package && !getPluginFn)
-      throw new RangeError('pluginInfo.package specified but getPluginFn not supplied!');
-
-    if(!specmd)
-      throw new TypeError('specmd not supplied');
-
     this.#plugin = pluginInfo && pluginInfo.package
       ? getPluginFn(pluginInfo.package)
       : null;
@@ -44,7 +75,7 @@ class SpecMDSpec {
     if(!this.#astPromise)
       this.parse();
 
-    return this.#specmd.html(this.specPath, this.#metadata);
+    return this.#specmd.html(this.#specPath, this.#metadata);
   }
 
   #pluginOutput = ()=>{
@@ -55,7 +86,7 @@ class SpecMDSpec {
       throw new Error('Plugin information not specified');
 
     const {args} = this.#pluginInfo;
-    return this.#plugin([this.specPath, ...args], this.#astPromise, this.#metadata);
+    return this.#plugin([this.#specPath, ...args], this.#astPromise, this.#metadata);
   }
 
   /**
@@ -65,7 +96,7 @@ class SpecMDSpec {
   output = async ()=>{
     let output = null;
 
-    if(this.#pluginInfo && this.#pluginInfo.package)
+    if(this.#plugin)
       output = await this.#pluginOutput();
     else
       output = await this.#htmlOutput();
