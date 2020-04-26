3bfdc8eb0d8850e164de309d91a0d50a7aadc4d0
Michael McCord <inquiries@michaelmccord.me>
2020-04-26 18:58:19 -0400

    feat(get-package): Minor changes to get-package to fix naming conflicts
    * Also adds documentation for get-package

--- src/get-package.js
+++ src/get-package.js
@@ -1,11 +1,37 @@
-module.exports = (require, requireg)=>(function(packageName) {
+/**
+ * requireg function
+ * @external requireg
+ * @see {@link https://github.com/h2non/requireg}
+ */
+const requireg = require('requireg');
+
+/**
+ * @module
+ */
+
+/**
+ * Gets {@link packageName} from local dependency cache or from a global install
+ * @typedef GetPackageReturnedFn
+ * @type {Function}
+ * @param {string} packageName
+*/
+
+
+/**
+ * Uses {@link requireFn} or {@link requiregFn} to get a [package]{@link packageName} from either the local dependency cache or from a global install
+ * @param  {Function} [requireFn = require] The function to use to retrieve from the local dependency cache (node_modules)
+ * @param  {Function} [requiregFn = requireg] The function to use to retrieve from a global installation (defaults to {@link external:requireg})
+ * @returns {GetPackageReturnedFn}
+ */
+module.exports = (requireFn = require, requiregFn = requireg)=>(
+function(packageName) {
   let specmd = null;
   try {
-    specmd = require(packageName);
+    specmd = requireFn(packageName);
   }catch(e) {}
 
   if(!specmd)
-    specmd = requireg(packageName);
+    specmd = requiregFn(packageName);
 
 
   return specmd;
