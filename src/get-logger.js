88d5c0ad9a8a3456e970f2a068cd9516cd710863
Michael McCord <inquiries@michaelmccord.me>
2020-04-26 18:57:08 -0400

    docs(get-logger): Add docs for get-logger module

--- src/get-logger.js
+++ src/get-logger.js
@@ -1,6 +1,21 @@
+/**
+ * Signale logger
+ * @external Signale
+ * @see {@link https://github.com/klaussinani/signale}
+ */
 const {Signale} = require('signale');
 const figures = require('figures');
 
+/** @module */
+
+/**
+ * Returns a {@link external:Signale} logger
+ * @param {Object} context Provides the {@link external:Signale} logger with the stdout and stderr streams
+ * @param {Stream} context.stdout The stdout stream
+ * @param {Stream} context.stderr The stderr stream
+ * @param {String} scope Provides a scope string to the {@link external:Signale} logger
+ * @returns {external:Signale}
+ */
 module.exports = ({stdout, stderr}, scope) =>
   new Signale({
     config: {displayTimestamp: true, underlineMessage: false, displayLabel: false},
